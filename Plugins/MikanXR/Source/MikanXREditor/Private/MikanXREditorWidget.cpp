846ec84808cd3ff9d97a786c4ea00ed96a066d46
Brendan Walker <brendan@millerwalker.net>
2024-12-13 23:54:50 -0800

    Remove checked-in version of Mikan Plugin

--- Plugins/MikanXR/Source/MikanXREditor/Private/MikanXREditorWidget.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022 Damian Nowakowski. All rights reserved.
-
-#include "MikanXREditorWidget.h"
-
-void UMikanXREditorWidget::TestButtonPressed()
-{
-	OnTestButtonPressedDelegate.ExecuteIfBound();
-}
